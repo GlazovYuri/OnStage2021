987094cbd3b7f7d23176636ca4ec29bbed0493b6
GlazovYuri <glazov.y0@gmail.com>
2021-10-31 15:57:17 +0300

    big update for double ledlights

--- effects.h
+++ effects.h
@@ -1,26 +1,75 @@
-#define NUMPIXELS 144
+float delta2_1;
+int place1 = rand() % 140 + 2;
+int size1 = rand() % 23 * 2 + 2;
 
-int color_mass1[NUMPIXELS][3];
-int color_mass2[NUMPIXELS][3];
+void pulse_rand1(float speed, int r1, int g1, int b1, int r2, int g2, int b2) {
 
-
-void static_gradient1(int r1, int g1, int b1, int r2, int g2, int b2) {
-  for (int i = 0; i < NUMPIXELS; i++) {
-
-    color_mass1[i][0] = r1 + (r2 - r1) * i / NUMPIXELS;
-    color_mass1[i][1] = g1 + (g2 - g1) * i / NUMPIXELS;
-    color_mass1[i][2] = b1 + (b2 - b1) * i / NUMPIXELS;
+  for (int i = 0; i < (place1 - size1 / 2); i++) {
+    pixels1.setPixelColor(i, pixels1.Color(r1, g1, b1));
   }
+
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels1.setPixelColor(i, pixels1.Color(r1, g1, b1));
+  }
+
+  delta2_1 = delta2_1 + speed;
+  if (delta2_1 >= 200) {
+    delta2_1 = 0;
+    place1 = rand() % 140 + 2;
+    size1  = rand() % 20 * 2 + 6;
+  }
+  int delta1_1 =  100 - abs(100 - delta2_1);
+
+  for (int i = 0; i < (size1 / 2); i++) {
+
+    pixels1.setPixelColor(place1 + i, pixels1.Color(r1 + ((r2 - r1) / (size1 / 2) * (size1 / 2 - i) * delta1_1 / 100), g1 + ((g2 - g1) / (size1 / 2) * (size1 / 2 - i) * delta1_1 / 100), b1 + ((b2 - b1) / (size1 / 2) * (size1 / 2 - i) * delta1_1 / 100)));
+    pixels1.setPixelColor(place1 - i, pixels1.Color(r1 + ((r2 - r1) / (size1 / 2) * (size1 / 2 - i) * delta1_1 / 100), g1 + ((g2 - g1) / (size1 / 2) * (size1 / 2 - i) * delta1_1 / 100), b1 + ((b2 - b1) / (size1 / 2) * (size1 / 2 - i) * delta1_1 / 100)));
+
+  }
+  //delay(20);
+  //Serial.println(delta);
+
 }
 
-void static_gradient2(int r1, int g1, int b1, int r2, int g2, int b2) {
-  for (int i = 0; i < NUMPIXELS; i++) {
 
-    color_mass2[i][0] = r1 + (r2 - r1) * i / NUMPIXELS;
-    color_mass2[i][1] = g1 + (g2 - g1) * i / NUMPIXELS;
-    color_mass2[i][2] = b1 + (b2 - b1) * i / NUMPIXELS;
+
+
+
+float delta2_2;
+int place2 = rand() % 140 + 2;
+int size2 = rand() % 23 * 2 + 2;
+
+void pulse_rand2(float speed, int r1, int g1, int b1, int r2, int g2, int b2) {
+
+  for (int i = 0; i < (place2 - size2 / 2); i++) {
+    pixels2.setPixelColor(i, pixels2.Color(r1, g1, b1));
   }
+
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels2.setPixelColor(i, pixels2.Color(r1, g1, b1));
+  }
+
+  delta2_2 = delta2_2 + speed;
+  if (delta2_2 >= 200) {
+    delta2_2 = 0;
+    place2 = rand() % 140 + 2;
+    size2  = rand() % 20 * 2 + 6;
+  }
+  int delta1_2 =  100 - abs(100 - delta2_2);
+
+  for (int i = 0; i < (size2 / 2); i++) {
+
+    pixels2.setPixelColor(place2 + i, pixels2.Color(r1 + ((r2 - r1) / (size2 / 2) * (size2 / 2 - i) * delta1_2 / 100), g1 + ((g2 - g1) / (size2 / 2) * (size2 / 2 - i) * delta1_2 / 100), b1 + ((b2 - b1) / (size2 / 2) * (size2 / 2 - i) * delta1_2 / 100)));
+    pixels2.setPixelColor(place2 - i, pixels2.Color(r1 + ((r2 - r1) / (size2 / 2) * (size2 / 2 - i) * delta1_2 / 100), g1 + ((g2 - g1) / (size2 / 2) * (size2 / 2 - i) * delta1_2 / 100), b1 + ((b2 - b1) / (size2 / 2) * (size2 / 2 - i) * delta1_2 / 100)));
+
+  }
+  //delay(20);
+  //Serial.println(delta);
+
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 float pos1;
 void dynamic_gradient1(float speed, int r1, int g1, int b1, int r2, int g2, int b2) {
@@ -29,13 +78,15 @@ void dynamic_gradient1(float speed, int r1, int g1, int b1, int r2, int g2, int 
 
 
   for (int i = 0; i < NUMPIXELS; i++) {
-
-    color_mass1[i][0] = r1 + (r2 - r1) / (NUMPIXELS / 2) * ((NUMPIXELS / 2) - abs(abs(pos1 - i) - (NUMPIXELS / 2)));
-    color_mass1[i][1] = g1 + (g2 - g1) / (NUMPIXELS / 2) * ((NUMPIXELS / 2) - abs(abs(pos1 - i) - (NUMPIXELS / 2)));
-    color_mass1[i][2] = b1 + (b2 - b1) / (NUMPIXELS / 2) * ((NUMPIXELS / 2) - abs(abs(pos1 - i) - (NUMPIXELS / 2)));
-
+    float k1 = ((NUMPIXELS / 2) - abs(abs(pos1 - i) - (NUMPIXELS / 2))) / (NUMPIXELS / 2);
+    float  r_1 = r1 + (r2 - r1) * k1;
+    float  g_1 = g1 + (g2 - g1) * k1;
+    float  b_1 = b1 + (b2 - b1) * k1;
+    //Serial.println(kr_1);
+    pixels1.setPixelColor(i, pixels1.Color(round(r_1), round(g_1), round(b_1)));
   }
 }
+
 
 float pos2;
 void dynamic_gradient2(float speed, int r1, int g1, int b1, int r2, int g2, int b2) {
@@ -44,60 +95,66 @@ void dynamic_gradient2(float speed, int r1, int g1, int b1, int r2, int g2, int 
 
 
   for (int i = 0; i < NUMPIXELS; i++) {
-
-    color_mass2[i][0] = r1 + (r2 - r1) / (NUMPIXELS / 2) * ((NUMPIXELS / 2) - abs(abs(pos1 - i) - (NUMPIXELS / 2)));
-    color_mass2[i][1] = g1 + (g2 - g1) / (NUMPIXELS / 2) * ((NUMPIXELS / 2) - abs(abs(pos1 - i) - (NUMPIXELS / 2)));
-    color_mass2[i][2] = b1 + (b2 - b1) / (NUMPIXELS / 2) * ((NUMPIXELS / 2) - abs(abs(pos1 - i) - (NUMPIXELS / 2)));
+    float k2 = ((NUMPIXELS / 2) - abs(abs(pos2 - i) - (NUMPIXELS / 2))) / (NUMPIXELS / 2);
+    float  r_2 = r1 + (r2 - r1) * k2;
+    float  g_2 = g1 + (g2 - g1) * k2;
+    float  b_2 = b1 + (b2 - b1) * k2;
+    pixels2.setPixelColor(i, pixels2.Color(round(r1 + (r2 - r1) / (NUMPIXELS / 2) * ((NUMPIXELS / 2) - abs(abs(pos2 - i) - (NUMPIXELS / 2)))), round(g1 + (g2 - g1) / (NUMPIXELS / 2) * ((NUMPIXELS / 2) - abs(abs(pos2 - i) - (NUMPIXELS / 2)))), round(b1 + (b2 - b1) / (NUMPIXELS / 2) * ((NUMPIXELS / 2) - abs(abs(pos2 - i) - (NUMPIXELS / 2))))));
 
   }
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
-float delta2;
-int place = rand() % 140 + 2;
-int size = rand() % 23 * 2 + 2;
-
-/*void pulse_rand(float speed, int r1, int g1, int b1, int r2, int g2, int b2) {
-
-  for (int i = 0; i < (place - size / 2); i++) {
-    pixels.setPixelColor(i, pixels.Color(r1, g1, b1));
+int inter1;
+void change_dynamic_gradient1(int interr, float speed, int r11, int g11, int b11, int r12, int g12, int b12, int r21, int g21, int b21, int r22, int g22, int b22) {
+  pos1 += speed;
+  inter1++;
+  if (inter1 < interr) {
+    changing_state1 = 1;
   }
+  else {
+    changing_state1 = 0;
+    inter1 = 0;                       //////need to move this line to global code
+  }
+
+  Serial.println("point1");
+  if (pos1 > NUMPIXELS)  pos1 -= NUMPIXELS;
 
   for (int i = 0; i < NUMPIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color(r1, g1, b1));
-  }
+    float  r1 = r11 + (r12 - r11) / (NUMPIXELS / 2) * ((NUMPIXELS / 2) - abs(abs(pos1 - i) - (NUMPIXELS / 2)));
+    float r_1 = r21 + (r22 - r21) / (NUMPIXELS / 2) * ((NUMPIXELS / 2) - abs(abs(pos1 - i) - (NUMPIXELS / 2)));
+    int  r__1 = round(r1  + (r_1 - r1)  * inter1 / interr);
 
-  delta2 = delta2 + speed;
-  if (delta2 >= 200) {
-    delta2 = 0;
-    place = rand() % 140 + 2;
-    size  = rand() % 20 * 2 + 6;
-  }
-  int delta =  100 - abs(100 - delta2);
+    float  g1 = g11 + (g12 - g11) / (NUMPIXELS / 2) * ((NUMPIXELS / 2) - abs(abs(pos1 - i) - (NUMPIXELS / 2)));
+    float g_1 = g21 + (g22 - g21) / (NUMPIXELS / 2) * ((NUMPIXELS / 2) - abs(abs(pos1 - i) - (NUMPIXELS / 2)));
+    int  g__1 = round(g1  + (g_1 - g1)  * inter1 / interr);
 
-  for (int i = 0; i < (size / 2); i++) {
+    float  b1 = b11 + (b12 - b11) / (NUMPIXELS / 2) * ((NUMPIXELS / 2) - abs(abs(pos1 - i) - (NUMPIXELS / 2)));
+    float b_1 = b21 + (b22 - b21) / (NUMPIXELS / 2) * ((NUMPIXELS / 2) - abs(abs(pos1 - i) - (NUMPIXELS / 2)));
+    int  b__1 = round(b1  + (b_1 - b1)  * inter1 / interr);
 
-    pixels.setPixelColor(place + i, pixels.Color(r1 + ((r2 - r1) / (size / 2) * (size / 2 - i) * delta / 100), g1 + ((g2 - g1) / (size / 2) * (size / 2 - i) * delta / 100), b1 + ((b2 - b1) / (size / 2) * (size / 2 - i) * delta / 100)));
-    pixels.setPixelColor(place - i, pixels.Color(r1 + ((r2 - r1) / (size / 2) * (size / 2 - i) * delta / 100), g1 + ((g2 - g1) / (size / 2) * (size / 2 - i) * delta / 100), b1 + ((b2 - b1) / (size / 2) * (size / 2 - i) * delta / 100)));
+    pixels1.setPixelColor(i, pixels1.Color(r__1, g__1, b__1));
 
   }
-  //delay(20);
-  pixels.show();
-  //Serial.println(delta);
+  Serial.println("point2");
 
-  }*/
+}
 
-/*
-  int change_flag;
-  int first_color;
-  int color_mass[NUMPIXELS];
-  int mass[3];
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  void change_to_gradient(int speed, int r1, int g1, int b1, int r2, int g2, int b2) { //setings for gradient
-  if (change_flag == 0) {
-    change_flag = 1;
-    for (int i = 0; i < NUMPIXELS; i++) {
-      int mass [0] = pixels.getPixelColor(mass{i]);
-    }
+
+/*int static_gradient(int r1, int g1, int b1, int r2, int g2, int b2) {
+  int color_mass[NUMPIXELS - 1][3];
+  for (int i = 0; i < NUMPIXELS; i++) {
+
+    color_mass[i][0] = r1 + (r2 - r1) / NUMPIXELS * i;
+    color_mass[i][1] = g1 + (g2 - g1) / NUMPIXELS * i;
+    color_mass[i][2] = b1 + (b2 - b1) / NUMPIXELS * i;
+
+    return color_mass;
+    //delay(DELAYVAL);
   }
   }*/
---
+++ led_light2.ino
@@ -0,0 +1,76 @@
+#include <Wire.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+
+#define PIN1        7 // On Trinket or Gemma, suggest changing this to 1
+#define PIN2        8
+
+#define NUMPIXELS 144 // Popular NeoPixel ring size
+
+int scene = 1;
+int changing_state1 = 0;
+int changing_state2 = 0;
+
+Adafruit_NeoPixel pixels1(NUMPIXELS, PIN1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels2(NUMPIXELS, PIN2, NEO_GRB + NEO_KHZ800);
+
+#include "effects.h"
+
+#define DELAYVAL 50 // Time (in milliseconds) to pause between pixels
+
+void changeScene(int howMany) {
+  while (1 < Wire.available()) { // loop through all but the last
+    char c = Wire.read(); // receive byte as a character
+    Serial.print(c);         // print the character
+  }
+  int x = Wire.read();    // receive byte as an integer
+  Serial.println(x);         // print the integer
+  scene = x;
+}
+
+void setup() {
+  Wire.begin(8);
+  Wire.onReceive(changeScene);
+
+  delay(123);
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  // END of Trinket-specific code.
+
+  pixels1.begin();
+  pixels2.begin();
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  int t1 = millis();
+  //pixels.clear(); // Set all pixel colors to 'off'
+  //dynamic_gradient1 (0.8, 255, 0, 0, 0, 0, 255);
+  //dynamic_gradient2 (0.8, 255 , 0, 0, 0, 255, 0);
+  //pixels.setPixelColor(59, 255, 255, 255);
+  //pulse_rand1 (0.8, 0, 0, 0, 0, 0, 75);
+  /*changing_state1 = 1;
+    switch (changing_state1) {
+    case (1):
+      //change_dynamic_gradient1(100, 0.8,  50, 0, 0,   0, 50, 0,   0, 0, 0,   0, 0, 50);
+      break;
+      case (0):
+        while (true) {
+          dynamic_gradient1(0.8,  0, 125, 0,   0, 0, 0);
+        }
+        break;
+    }*/
+  Serial.println("point0");
+  dynamic_gradient1(0.8,  0, 255, 0, 255, 0, 0);
+  dynamic_gradient2(0.8,  25, 0, 255, 255, 0, 25);
+  pixels1.show();
+  pixels2.show();
+
+  //Serial.println();
+
+}
